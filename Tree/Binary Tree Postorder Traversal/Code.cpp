18cb07e42678cb96f052c20f12da006a7191a3f3
Bhargavee Singh <bhargavee090@gmail.com>
2025-06-14 20:33:59 +0530

    Create Code.cpp

---
+++ Tree/Binary Tree Postorder Traversal/Code.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int> &arr){
+        if(root == NULL){
+            return;
+        }
+        helper(root->left,arr);
+        helper(root->right,arr);
+        arr.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        helper(root, arr);
+        return arr;
+        
+    }
+};
